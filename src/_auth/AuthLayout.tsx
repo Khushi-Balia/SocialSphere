0f06d7bc43ab83eed8ffe70fa9ca4e6099567233
Khushi-Balia <khhbalia@gmail.com>
2023-11-29 00:54:54 +0530

    Completed user authentication using appwrite

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Outlet, Navigate} from 'react-router-dom'
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+  return (
+    <>
+    {isAuthenticated? (
+      <Navigate to="/" />
+    )
+      : (
+        <>
+        <section className='flex flex-1 justify-center items-center flex-col py-10'>
+          <Outlet />
+        </section>
+
+        <img src='\assets\images\neon.webp'
+        alt='logo'
+        className='hidden xl:block h-screen w-1/3 object-cover bg-no-repeat'
+        />
+        </>
+      )
+    }    
+
+    </>
+   
+  )
+}
+
+export default AuthLayout
---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='flex-center w-full'>
+        <img src="\assets\icons\loader.svg"
+        alt='load'
+        width = {24}
+        height={24}></img>
+    </div>
+  )
+}
+
+export default Loader
